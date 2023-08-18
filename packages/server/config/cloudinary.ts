abdaa93dc4bcc64c378b5fcc5b8133e772cdaf4d
Ashutosh <ashuma2721@gmail.com>
2023-08-18 22:48:59 +0530

    server: removed console

--- packages/server/config/cloudinary.ts
+++ packages/server/config/cloudinary.ts
@@ -1,8 +1,5 @@
 import '@/utils/load-env';
 
-// eslint-disable-next-line no-console
-console.log('process.env.CLOUDINARY_CLOUD_NAME', process.env.CLOUDINARY_CLOUD_NAME);
-
 export const cloudinaryConfig = {
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
